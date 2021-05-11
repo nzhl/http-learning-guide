00e98d57a07e31c435158ec6e4ef37d53ef07d79
zhangzhimin <zhangzhimin.hardcore@bytedance.com>
2021-05-11 13:56:17 +0800

    feat: iframe hash 跨域

--- src/example3/server.ts
+++ src/example3/server.ts
@@ -10,24 +10,16 @@ export const startServer = (port: number, host: string, cb: () => void) => {
     d(`=> ${req.url}`);
 
     match(req)
-      .with({ url: '/index' }, () => {
-        const htmlStream = createReadStream('./dist/index.html');
-        htmlStream.pipe(res);
-      })
       .with({ url: '/api/info' }, () => {
         res.end(JSON.stringify({ name: 'Fred', age: 25 }));
       })
-      .with({ url: '/iframe-name' }, () => {
-        const htmlStream = createReadStream('./dist/iframe-name.html');
-        htmlStream.pipe(res);
-      })
-      .with({ url: '/iframe-hash' }, () => {
-        const htmlStream = createReadStream('./dist/iframe-hash.html');
-        htmlStream.pipe(res);
-      })
       .with(__, () => {
-        const htmlStream = createReadStream('./dist/404.html');
+        const htmlStream = createReadStream(`./dist/${req.url}.html`);
         htmlStream.pipe(res);
+        htmlStream.on('error', () => {
+          const htmlStream = createReadStream(`./dist/404.html`);
+          htmlStream.pipe(res);
+        });
       })
       .exhaustive();
   });
